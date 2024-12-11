5fc1ed95fcfd9a9bc23d762337a1c04e3b88765d
damiangarasz <damian.garasz@gmail.com>
2024-12-11 17:11:06 +0100

    nic nie działą

--- src/ts/Games/Game-1.tsx
+++ src/ts/Games/Game-1.tsx
@@ -1,8 +1,36 @@
-import { useEffect, useState } from "react";
+import { useState, useRef } from "react";
+import { ImageCache } from "./ImageCache";
 
-const [picArr, picArrFn] = useState([]);
+const canvas = document.querySelector("#game_canvas");
+const ctx = canvas.getContext("2d");
+
+const imageCache = ImageCache();
+
+// ----------- Tło początek
+
+class srodowisko {
+  constructor() {
+    this.pierwszy = -50;
+    this.drugi = -50;
+    this.trzeci = -50;
+  }
+  drawPierwszy() {
+    ctx.drawImage(pierwszyPlan, this.pierwszy, -245, 20000, 400);
+  }
+
+  drawDrugi() {
+    ctx.drawImage(drugiPlan, this.drugi, -270);
+  }
+  drawTrzeci() {
+    ctx.drawImage(trzeciPlan, this.trzeci, -50);
+  }
+}
+const tlo = new srodowisko();
+
+// ----------- Tło koniec
 
 export function Game1() {
+  console.log(imageCache.background);
   return (
     <div className="board">
       <canvas className="game_canvas mx-auto mt-10 w-[750px] border border-black"></canvas>
---
+++ src/ts/Games/ImageCache.tsx
@@ -0,0 +1,36 @@
+const imageCache: { [key: string]: HTMLImageElement } = {};
+
+const imageUrls = {
+  background:
+    "https://raw.githubusercontent.com/damiangarasz/assets/main/Legacy-Fantasy%20-%20High%20Forest%202.3/Background/Background.png",
+  pierwszyPlan:
+    "https://raw.githubusercontent.com/damiangarasz/assets/main/Legacy-Fantasy%20-%20High%20Forest%202.3/gotowce/pierwszy-plan.png",
+  trzeciPlan:
+    "https://raw.githubusercontent.com/damiangarasz/assets/main/Legacy-Fantasy%20-%20High%20Forest%202.3/gotowce/trzeci-plan.png",
+  drugiPlan:
+    "https://raw.githubusercontent.com/damiangarasz/assets/main/Legacy-Fantasy%20-%20High%20Forest%202.3/gotowce/drugi-plan.png",
+  heroIdle:
+    "https://raw.githubusercontent.com/damiangarasz/assets/main/Legacy-Fantasy%20-%20High%20Forest%202.3/Character/Idle/Idle-Sheet.png",
+  heroWalkR:
+    "https://raw.githubusercontent.com/damiangarasz/assets/main/Legacy-Fantasy%20-%20High%20Forest%202.3/Character/Run/Run-R.png",
+  heroWalkL:
+    "https://raw.githubusercontent.com/damiangarasz/assets/main/Legacy-Fantasy%20-%20High%20Forest%202.3/Character/Run/Run-L.png",
+  heroJump:
+    "https://raw.githubusercontent.com/damiangarasz/assets/main/Legacy-Fantasy%20-%20High%20Forest%202.3/Character/Jumlp-All/Jump-All-Sheet.png",
+  heroDead:
+    "https://raw.githubusercontent.com/damiangarasz/assets/main/Legacy-Fantasy%20-%20High%20Forest%202.3/Character/Dead/Dead-Sheet.png",
+  beeIdle:
+    "https://raw.githubusercontent.com/damiangarasz/assets/main/Legacy-Fantasy%20-%20High%20Forest%202.3/Mob/Small%20Bee/Fly/Fly-Sheet.png",
+  heroAttack:
+    "https://raw.githubusercontent.com/damiangarasz/assets/main/Legacy-Fantasy%20-%20High%20Forest%202.3/Character/Attack-01/Attack-01-Sheet.png",
+};
+
+Object.entries(imageUrls).forEach(([key, url]) => {
+  const img = new Image();
+  img.src = url;
+  imageCache[key] = img;
+});
+
+export function ImageCache() {
+  return imageCache;
+}
