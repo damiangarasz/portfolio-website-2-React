767915ceb1b1c459f47ee994ff2857248f173610
damiangarasz <damian.garasz@gmail.com>
2025-01-14 18:37:26 +0100

    nic nie rozumiem

--- src/ts/App.tsx
+++ src/ts/App.tsx
@@ -3,6 +3,7 @@ import ErrorBoundary from "./ErrorBoundary";
 import Header from "./Header/Header";
 import { Form } from "./Sing-in/Form";
 import { Game1 } from "./Games/Game-1/Game-1";
+import { Game2 } from "./Games/Game-2/Game-2";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -19,6 +20,9 @@ const App = () => {
           </Routes>
           <Routes>
             <Route path="/game-1" element={<Game1 />}></Route>
+          </Routes>
+          <Routes>
+            <Route path="/game-2" element={<Game2 />}></Route>
           </Routes>
         </QueryClientProvider>
       </BrowserRouter>
--- src/ts/Games/Game-2/Game-2.tsx
+++ src/ts/Games/Game-2/Game-2.tsx
@@ -1,4 +1,88 @@
+import { useEffect } from "react";
 
-export function Game2(){
-    return (<div></div>)
+export function Game2() {
+  useEffect(() => {
+    const img = document.getElementById("myImage");
+
+    if (img != null) {
+      img.addEventListener("dragstart", (event) => {
+        if (!event.dataTransfer) {
+          return;
+        }
+        console.log(img);
+        event.dataTransfer.setDragImage(img, 15, 15);
+      });
+    }
+  });
+
+  return (
+    <div className="chess-grid mx-auto h-[250px] w-[250px]">
+      <div>
+        <img id="myImage" src="./img/Game-2/bb.png" />
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 }
