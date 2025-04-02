b506415b4f4a1dfde1c5f0864802b90b610dbaff
damiangarasz <damian.garasz@gmail.com>
2025-04-02 18:46:18 +0200

    meh

--- src/ts/Games/Game-2/Game-2.tsx
+++ src/ts/Games/Game-2/Game-2.tsx
@@ -60,52 +60,52 @@ export function Game2() {
   return (
     <div className="chess-grid mx-auto" style={rozmiarOkna}>
       <div id="s1" className="relative">
-        <img className="myImage" src="/img/Game-2/br.png" />
+        <img className="myImage" src="./img/Game-2/br.png" />
       </div>
       <div id="s2" className="relative">
-        <img className="myImage" src="/img/Game-2/bn.png" />
+        <img className="myImage" src="./img/Game-2/bn.png" />
       </div>
       <div id="s3" className="relative">
-        <img className="myImage" src="/img/Game-2/bb.png" />
+        <img className="myImage" src="./img/Game-2/bb.png" />
       </div>
       <div id="s4" className="relative">
-        <img className="myImage" src="/img/Game-2/bq.png" />
+        <img className="myImage" src="./img/Game-2/bq.png" />
       </div>
       <div id="s5" className="relative">
-        <img className="myImage" src="/img/Game-2/bk.png" />
+        <img className="myImage" src="./img/Game-2/bk.png" />
       </div>
       <div id="s6" className="relative">
-        <img className="myImage" src="/img/Game-2/bb.png" />
+        <img className="myImage" src="./img/Game-2/bb.png" />
       </div>
       <div id="s7" className="relative">
-        <img className="myImage" src="/img/Game-2/bn.png" />
+        <img className="myImage" src="./img/Game-2/bn.png" />
       </div>
       <div id="s8" className="relative">
-        <img className="myImage" src="/img/Game-2/br.png" />
+        <img className="myImage" src="./img/Game-2/br.png" />
       </div>
       <div id="s9" className="relative">
-        <img className="myImage" src="/img/Game-2/bp.png" />
+        <img className="myImage" src="./img/Game-2/bp.png" />
       </div>
       <div id="s10" className="relative">
-        <img className="myImage" src="/img/Game-2/bp.png" />
+        <img className="myImage" src="./img/Game-2/bp.png" />
       </div>
       <div id="s11" className="relative">
-        <img className="myImage" src="/img/Game-2/bp.png" />
+        <img className="myImage" src="./img/Game-2/bp.png" />
       </div>
       <div id="s12" className="relative">
-        <img className="myImage" src="/img/Game-2/bp.png" />
+        <img className="myImage" src="./img/Game-2/bp.png" />
       </div>
       <div id="s13" className="relative">
-        <img className="myImage" src="/img/Game-2/bp.png" />
+        <img className="myImage" src="./img/Game-2/bp.png" />
       </div>
       <div id="s14" className="relative">
-        <img className="myImage" src="/img/Game-2/bp.png" />
+        <img className="myImage" src="./img/Game-2/bp.png" />
       </div>
       <div id="s15" className="relative">
-        <img className="myImage" src="/img/Game-2/bp.png" />
+        <img className="myImage" src="./img/Game-2/bp.png" />
       </div>
       <div id="s16" className="relative">
-        <img className="myImage" src="/img/Game-2/bp.png" />
+        <img className="myImage" src="./img/Game-2/bp.png" />
       </div>
       <div id="s17" className="relative"></div>
       <div id="s18" className="relative"></div>
@@ -140,52 +140,52 @@ export function Game2() {
       <div id="s47" className="relative"></div>
       <div id="s48" className="relative"></div>
       <div id="s49" className="relative">
-        <img className="myImage" src="/img/Game-2/wp.png" />
+        <img className="myImage" src="./img/Game-2/wp.png" />
       </div>
       <div id="s50" className="relative">
-        <img className="myImage" src="/img/Game-2/wp.png" />
+        <img className="myImage" src="./img/Game-2/wp.png" />
       </div>
       <div id="s51" className="relative">
-        <img className="myImage" src="/img/Game-2/wp.png" />
+        <img className="myImage" src="./img/Game-2/wp.png" />
       </div>
       <div id="s52" className="relative">
-        <img className="myImage" src="/img/Game-2/wp.png" />
+        <img className="myImage" src="./img/Game-2/wp.png" />
       </div>
       <div id="s53" className="relative">
-        <img className="myImage" src="/img/Game-2/wp.png" />
+        <img className="myImage" src="./img/Game-2/wp.png" />
       </div>
       <div id="s54" className="relative">
-        <img className="myImage" src="/img/Game-2/wp.png" />
+        <img className="myImage" src="./img/Game-2/wp.png" />
       </div>
       <div id="s55" className="relative">
-        <img className="myImage" src="/img/Game-2/wp.png" />
+        <img className="myImage" src="./img/Game-2/wp.png" />
       </div>
       <div id="s56" className="relative">
-        <img className="myImage" src="/img/Game-2/wp.png" />
+        <img className="myImage" src="./img/Game-2/wp.png" />
       </div>
       <div id="s57" className="relative">
-        <img className="myImage" src="/img/Game-2/wr.png" />
+        <img className="myImage" src="./img/Game-2/wr.png" />
       </div>
       <div id="s58" className="relative">
-        <img className="myImage" src="/img/Game-2/wn.png" />
+        <img className="myImage" src="./img/Game-2/wn.png" />
       </div>
       <div id="s59" className="relative">
-        <img className="myImage" src="/img/Game-2/wb.png" />
+        <img className="myImage" src="./img/Game-2/wb.png" />
       </div>
       <div id="s60" className="relative">
-        <img className="myImage" src="/img/Game-2/wq.png" />
+        <img className="myImage" src="./img/Game-2/wq.png" />
       </div>
       <div id="s61" className="relative">
-        <img className="myImage" src="/img/Game-2/wk.png" />
+        <img className="myImage" src="./img/Game-2/wk.png" />
       </div>
       <div id="s62" className="relative">
-        <img className="myImage" src="/img/Game-2/wb.png" />
+        <img className="myImage" src="./img/Game-2/wb.png" />
       </div>
       <div id="s63" className="relative">
-        <img className="myImage" src="/img/Game-2/wn.png" />
+        <img className="myImage" src="./img/Game-2/wn.png" />
       </div>
       <div id="s64" className="relative">
-        <img className="myImage" src="/img/Game-2/wr.png" />
+        <img className="myImage" src="./img/Game-2/wr.png" />
       </div>
     </div>
   );
