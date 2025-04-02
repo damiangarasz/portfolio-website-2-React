57b860cb1d66907122553328f527d6363d9e6cd0
damiangarasz <damian.garasz@gmail.com>
2025-04-02 18:40:17 +0200

    dobijcie mnie

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import postcss from "./postcss.config.cjs";
 
 export default defineConfig(({ mode }) => {
   // Ładowanie zmiennych środowiskowych
@@ -7,13 +8,16 @@ export default defineConfig(({ mode }) => {
 
   return {
     root: "src",
-    plugins: [react()],
+    plugins: [
+      react(),
+    ],
     define: {
       "process.env": { ...process.env, ...env }, // Dodajemy zmienne środowiskowe
     },
     base: "./", // Zapewnia poprawne odniesienia do plików statycznych
     build: {
       outDir: "../docs",
+      emptyOutDir: true,
     },
     server: {
       open: true,
