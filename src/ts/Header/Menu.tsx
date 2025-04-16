98c1400379c3954c3a3bbcc08cbe99e553a9ce87
damiangarasz <damian.garasz@gmail.com>
2025-04-16 14:45:24 +0200

    twikuje

--- src/ts/Header/Menu.tsx
+++ src/ts/Header/Menu.tsx
@@ -72,7 +72,7 @@ const Menu = () => {
         </svg>
       </div>
       <nav aria-label="main menu hover:color" aria-expanded="false">
-        <ol className="main-menu cursor-pointer space-x-5">
+        <ol className="main-menu mx-md:flex cursor-pointer space-x-5">
           <li>
             <span className="dot">•</span>
             Home
