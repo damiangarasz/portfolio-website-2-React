c9ee41028f81102ee9b54247ce6035f7347f8e23
damiangarasz <damian.garasz@gmail.com>
2025-04-17 11:37:10 +0200

    ci sie niefasfsa

--- src/ts/Header/Header.tsx
+++ src/ts/Header/Header.tsx
@@ -29,9 +29,7 @@ const Header = () => {
             ></path>
           </g>
         </svg>
-        <span className="lol max-md:text-blue-600 md:text-red-600">
-          Damian Garasz
-        </span>
+        <span className="lol max-md:text-blue-600 md:text-red-600">DUPA</span>
       </div>
       <Menu />
       <SearchBar />
