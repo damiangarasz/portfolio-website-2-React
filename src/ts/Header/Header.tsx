4bf930563c862d9dc5f8c7235327506d4da46ad0
damiangarasz <damian.garasz@gmail.com>
2025-04-17 11:33:36 +0200

    co sie dzieje

--- src/ts/Header/Header.tsx
+++ src/ts/Header/Header.tsx
@@ -5,7 +5,7 @@ import SearchBar from "./SearchBar";
 const Header = () => {
   return (
     <header className="relative mx-10 mt-7 flex justify-between font-medium">
-      <div className="flex w-[150px]">
+      <div className="lol flex w-[150px] max-md:text-blue-600 md:text-red-600">
         <svg
           className="w-6"
           viewBox="0 0 16 16"
