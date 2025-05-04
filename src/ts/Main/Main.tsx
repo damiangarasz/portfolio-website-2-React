755fee1e2baf479beea1478e6a9741faa021c2ec
Damian Garasz <damian.garasz@gmail.com>
2025-05-04 16:49:28 +0200

    lol text

--- src/ts/Main/Main.tsx
+++ src/ts/Main/Main.tsx
@@ -1,8 +1,17 @@
+import { useEffect, useRef } from "react";
+
 export function Main() {
+
+    
+    const textSzachy = `Mój najnowszy projekt to samodzielnie zakodowana aplikacja szachowa. Opracowałem dla niej autorski, podstawowy silnik, który zarządza mechaniką gry, uwzględniając specjalne ruchy (roszada, bicie w przelocie) i kontrolujący legalność posunięć. Wykorzystałem API ChatGPT jako AI przeciwnika, który gra czarnymi`
+    
+
+    
+
     return (
         <main className="kontener">
-            <div></div>
-            <div><video><source src="img/main/szachy-gotowy.mp4" type="video/mp4"></source></video></div>
+            <div><span>{textSzachy}</span></div>
+            <div><video autoPlay muted><source src="img/main/szachy-gotowy.mp4" type="video/mp4"></source></video></div>
             <div><img src="img/main/1.webp
             "></img></div>
             <div></div>
