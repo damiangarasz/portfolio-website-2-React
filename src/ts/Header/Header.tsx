68aa5efc4a36908ecc4b4f9b2535713e3d56548d
Damian Garasz <damian.garasz@gmail.com>
2025-05-04 22:39:33 +0200

    sladkj

--- src/ts/Header/Header.tsx
+++ src/ts/Header/Header.tsx
@@ -5,7 +5,7 @@ import SearchBar from "./SearchBar";
 const Header = () => {
   return (
     <header className="relative mx-10 mt-7 flex justify-between font-medium">
-      <div className="flex w-[150px]">
+      <div className="flex w-[160px]">
         <svg
           className="w-6"
           viewBox="0 0 16 16"
