01822c22d5507a884a7634c86ccf061895c2130a
Damian Garasz <damian.garasz@gmail.com>
2025-05-04 14:53:42 +0200

    lol video

--- src/ts/Main/Main.tsx
+++ src/ts/Main/Main.tsx
@@ -2,7 +2,7 @@ export function Main() {
     return (
         <main className="kontener">
             <div></div>
-            <div><video autoPlay></video><source src="https://raw.githubusercontent.com/damiangarasz/portfolio-website-2-React/refs/heads/main/docs/main/szachy-gotowy.mp4" itemType="video/mp4"></source></div>
+            <div><video width="320" height="240" autoPlay></video><source src="https://raw.githubusercontent.com/damiangarasz/portfolio-website-2-React/refs/heads/main/docs/main/szachy-gotowy.mp4" itemType="video/mp4"></source></div>
             <div></div>
             <div></div>
             <div></div>
