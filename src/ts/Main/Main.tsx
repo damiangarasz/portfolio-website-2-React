d7d764bffe113d16d56f90e351d3837f35509ee9
Damian Garasz <damian.garasz@gmail.com>
2025-05-04 15:55:02 +0200

    wideo:

--- src/ts/Main/Main.tsx
+++ src/ts/Main/Main.tsx
@@ -2,7 +2,7 @@ export function Main() {
     return (
         <main className="kontener">
             <div></div>
-            <div><video width="320" height="240" muted autoPlay><source src="https://youtu.be/Gzbf3lZXYzk?si=PAd3tLec2lCu8XPG" type="video/mp4"></source></video></div>
+            <div><iframe width="560" height="315" src="https://www.youtube.com/embed/Gzbf3lZXYzk?si=PAd3tLec2lCu8XPG&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share loop=1 mute=1" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>
             <div><img src="https://github.com/damiangarasz/portfolio-website-2-React/raw/refs/heads/gh-pages/src/public/img/main/1.webp
             "></img></div>
             <div></div>
